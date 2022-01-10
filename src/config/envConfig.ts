523720bcb43932b0cf02c439b20d2c018bd15f13
Krisliltech <mgbekwutechristian91@gmail.com>
2022-01-10 16:41:11 +0100

    refactoring

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,8 +1,6 @@
 import {config} from "dotenv"
 config(); // activate access to environment variables.
 const env = {
-    REDIS_HOST: process.env.REDIS_HOST,
-    RESIS_PORT: process.env.REDIS_PORT,
     INBOUND_URL: process.env.INBOUND_URL,
     OUTBOUND_URL: process.env.OUTBOUND_URL
 }
